4a6ad799442432688232759fd8f5d109dea4800c
aniket-mantra <aniket.s@mantralabsglobal.com>
2022-12-21 22:14:31 +0530

    implement a cros cookies issue

--- application.go
+++ application.go
@@ -4,8 +4,18 @@ import (
 	"net/http"
 
 	"github.com/aniket0951/Chatrapati-Maharaj/routers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
+
+func CORSConfig() cors.Config {
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"http://localhost:3000"}
+	corsConfig.AllowCredentials = true
+	corsConfig.AddAllowHeaders("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers", "Content-Type", "X-XSRF-TOKEN", "Accept", "Origin", "X-Requested-With", "Authorization")
+	corsConfig.AddAllowMethods("GET", "POST", "PUT", "DELETE")
+	return corsConfig
+}
 
 const (
 	ContentTypeBinary = "application/octet-stream"
@@ -17,6 +27,7 @@ const (
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.New(CORSConfig()))
 
 	router.Static("static", "static")
 
